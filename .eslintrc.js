9df4923997dab00d2bbb5d641b83676e49f7e99c
Michael Coakley <mcoakley@acmeframework.com>
2021-02-19 14:55:16 -0500

    Added env and globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   extends: ['eslint:recommended', 'prettier'],
+  globals: {
+    chrome: true,
+    QRCode: true,
+  },
 };
